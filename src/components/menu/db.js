7acfdc3f7ce6ae96a0a561739ef2ca348eb429b0
Mohamed <mh3890464@gmail.com>
2021-10-31 23:13:41 +0200

    change db

---
+++ src/components/menu/db.js
@@ -0,0 +1,116 @@
+const menu = [
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/7613432/pexels-photo-7613432.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    name: "Beef",
+    price: 30,
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/7741852/pexels-photo-7741852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Stack",
+    price: 40,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/1105325/bbq-meet-eating-diner-1105325.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Meat",
+    price: 15,
+  },
+  {
+    id: 4,
+    image:
+      "https://images.pexels.com/photos/410648/pexels-photo-410648.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Reiash",
+    price: 20,
+  },
+  {
+    id: 5,
+    image:
+      "https://images.pexels.com/photos/2983098/pexels-photo-2983098.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Burger Beef",
+    price: 40,
+  },
+  {
+    id: 6,
+    image:
+      "https://images.pexels.com/photos/3219547/pexels-photo-3219547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Burger Beef",
+    price: 30,
+  },
+  {
+    id: 7,
+    image:
+      "https://images.pexels.com/photos/3052362/pexels-photo-3052362.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Double Burger",
+    price: 55,
+  },
+  {
+    id: 8,
+    image:
+      "https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Burger",
+    price: 25,
+  },
+  {
+    id: 9,
+    image:
+      "https://images.pexels.com/photos/1516415/pexels-photo-1516415.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "File",
+    price: 20,
+  },
+  {
+    id: 10,
+    image:
+      "https://images.pexels.com/photos/725991/pexels-photo-725991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Fish",
+    price: 25,
+  },
+  {
+    id: 11,
+    image:
+      "https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Sea Food",
+    price: 40,
+  },
+  {
+    id: 12,
+    image:
+      "https://images.pexels.com/photos/858508/pexels-photo-858508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Soushi",
+    price: 30,
+  },
+  {
+    id: 13,
+    image:
+      "https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Pizza",
+    price: 30,
+  },
+  {
+    id: 14,
+    image:
+      "https://images.pexels.com/photos/2741457/pexels-photo-2741457.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Chesse Pizza",
+    price: 20,
+  },
+  {
+    id: 15,
+    image:
+      "https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Margrina",
+    price: 35,
+  },
+  {
+    id: 16,
+    image:
+      "https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    name: "Pizza Hot",
+    price: 40,
+  },
+];
+
+export default menu;
